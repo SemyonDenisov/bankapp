73ae9b196c73a07ca61a1af6e15db3b3966d0d1e
SemyonDenisov <semdenis2003@mail.ru>
2025-11-18 13:22:59 +0400

    fix test-env

--- accounts-microservice/src/test/java/ru/yandex/account/integration/service/NotificationServiceIntegrationTests.java
+++ accounts-microservice/src/test/java/ru/yandex/account/integration/service/NotificationServiceIntegrationTests.java
@@ -3,6 +3,7 @@ package ru.yandex.account.integration.service;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -38,6 +39,11 @@ class NotificationServiceIntegrationTests {
 
     @Autowired
     private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @BeforeAll
+    static void init(@Autowired EmbeddedKafkaBroker broker) {
+        System.setProperty("spring.kafka.bootstrap-servers", broker.getBrokersAsString());
+    }
 
     @Test
     void testSendNotification() {
